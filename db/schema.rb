0b28a4549c1d32bb533e7776110e6c3a6829f407
Tushar <singhtushar348@gmail.com>
2022-11-16 14:08:12 +0530

    Update schema with devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_115_140_021) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_165226) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name'
-    t.string 'icon'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'author_id'
-    t.index ['author_id'], name: 'index_categories_on_author_id'
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_categories_on_author_id"
   end
 
-  create_table 'categories_payments', id: false, force: :cascade do |t|
-    t.bigint 'category_id', null: false
-    t.bigint 'payment_id', null: false
-    t.index %w[category_id payment_id], name: 'index_categories_payments_on_category_id_and_payment_id'
+  create_table "categories_payments", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "payment_id", null: false
+    t.index ["category_id", "payment_id"], name: "index_categories_payments_on_category_id_and_payment_id"
   end
 
-  create_table 'payments', force: :cascade do |t|
-    t.string 'name'
-    t.float 'amount'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'author_id'
-    t.index ['author_id'], name: 'index_payments_on_author_id'
+  create_table "payments", force: :cascade do |t|
+    t.string "name"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_payments_on_author_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'categories', 'users', column: 'author_id'
-  add_foreign_key 'payments', 'users', column: 'author_id'
+  add_foreign_key "categories", "users", column: "author_id"
+  add_foreign_key "payments", "users", column: "author_id"
 end
