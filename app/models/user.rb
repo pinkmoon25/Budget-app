b01f44a4cefcdfdbd66ff21732cdcd4f111e84d2
Tushar <singhtushar348@gmail.com>
2022-11-16 14:04:21 +0530

    Add devise modules to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :categories, foreign_key: 'author_id', dependent: :destroy
   has_many :payments, foreign_key: 'author_id', dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50,
+                                             too_long: '%<count>s characters is the maximum allowed' }
 end
