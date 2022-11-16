b3821b4d7e4be0b765d23d3c3d4324a1c994baa5
Tushar <singhtushar348@gmail.com>
2022-11-16 14:03:04 +0530

    Add validations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,7 @@ class Payment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_and_belongs_to_many :categories
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 100,
+                                             too_long: '%<count>s characters is the maximum allowed' }
   validates :amount, numericality: true
 end
