626ce578d9359215b715ae5225857cd43c1180e5
Tushar <singhtushar348@gmail.com>
2022-11-16 15:12:36 +0530

    Install and configure bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :recipes, except: :update do
+    resources :recipe_foods, except: :update
+  end
   
 end
