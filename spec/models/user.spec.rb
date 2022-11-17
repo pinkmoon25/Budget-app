a98e3ad5fcd7daa4af91a8ff7d2d04a31e5eea0f
Tushar <singhtushar348@gmail.com>
2022-11-18 00:18:30 +0530

    Remove default generated helpers for home controller

--- spec/models/user.spec.rb
+++ spec/models/user.spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
     expect(User.new).to_not be_valid
   end
   it 'requires a name attribute' do
-    user = User.new(name: 'Test')
+    user = User.new(name: 'Test', email: 'tushar@mail.com', password: 'budget')
     expect(user).to be_valid
   end
 end
