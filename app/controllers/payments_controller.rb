51735ec394a46464bccb1f2519dfe11660088a08
Tushar <singhtushar348@gmail.com>
2022-11-17 15:17:49 +0530

    Add multiple categories selec option for transactions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,8 +8,6 @@ class PaymentsController < ApplicationController
   def create
     @payment = Payment.new(payment_params)
     @payment.author = current_user
-    @category = Category.find(params[:category_id])
-    @payment.categories << @category
     if @payment.save
       flash[:notice] = 'Transaction added successfully'
       redirect_to category_path(params[:category_id])
@@ -22,6 +20,6 @@ class PaymentsController < ApplicationController
   private
 
   def payment_params
-    params.require(:payment).permit(:name, :amount)
+    params.require(:payment).permit(:name, :amount, category_ids: [])
   end
 end
